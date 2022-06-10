82c68fcae2b9e5c78e01a73bfa717aed8ff805da
vear <vear.vip@qq.com>
2022-06-11 00:56:46 +0800

    feat: first commit

---
+++ index.d.ts
@@ -0,0 +1,43 @@
+import { useState, useEffect, useRef } from 'react'
+
+declare module 'giao-hook'
+
+export function useCallbackState(initState) {
+  const callbackRef = useRef();
+  const [data, setData] = useState(initState);
+
+  useEffect(() => {
+    callbackRef.current && callbackRef.current(data);
+  }, [data]);
+
+  return [
+    data,
+    function (value, callback) {
+      callbackRef.current = callback;
+      setData(value);
+    }
+  ];
+}
+
+export function useAsyncGetState(initState) {
+  const dataRef = useRef(initState);
+  const [data, setData] = useCallbackState(initState);
+
+  return [
+    data,
+    function (arg, callback) {
+      return new Promise((resolve) => {
+        if (typeof arg === 'function') {
+          dataRef.current = arg(data)
+          setData(arg(data), (_data) => resolve(_data))
+          callback && callback(arg(data))
+        } else {
+          dataRef.current = arg
+          setData(arg, (_data) => resolve(_data))
+          callback && callback(arg)
+        }
+      })
+    },
+    () => dataRef.current
+  ];
+}
